fdb4d3972d0f513baad717cfb1ffb856de930933
Denis <suboch.denny@gmail.com>
2020-03-13 14:12:25 +0300

    feat:implement activation phone screen after click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuLinkList = document.querySelector(".desktop-menu__list")
 let menuLinks = menuLinkList.querySelectorAll(".desktop-menu__link")
-menuLinkList.addEventListener("click", function(event)  {
-    if (event.target.tagName=='A') {
+menuLinkList.addEventListener("click", function (event) {
+    if (event.target.tagName == 'A') {
         for (let link of menuLinks) {
             if (link.classList.contains("desktop-menu__link--active")) {
                 link.classList.remove("desktop-menu__link--active")
@@ -15,78 +15,102 @@ menuLinkList.addEventListener("click", function(event)  {
 
 
 let portfolioImages = [
-    {"src" : "assets/images/portfolio/img-1.png",
-    "alt": "img 1"},
-    {"src" : "assets/images/portfolio/img-2.png",
-    "alt": "img 2"},
-    {"src" : "assets/images/portfolio/img-3.png",
-    "alt": "img 3"},
-    {"src" : "assets/images/portfolio/img-4.png",
-    "alt": "img 4"},
-    {"src" : "assets/images/portfolio/img-5.png",
-    "alt": "img 5"},
-    {"src" : "assets/images/portfolio/img-6.png",
-    "alt": "img 6"},
-    {"src" : "assets/images/portfolio/img-7.png",
-    "alt": "img 7"},
-    {"src" : "assets/images/portfolio/img-8.png",
-    "alt": "img 8"},
-    {"src" : "assets/images/portfolio/img-9.png",
-    "alt": "img 9"},
-    {"src" : "assets/images/portfolio/img-10.png",
-    "alt": "img 10"},
-    {"src" : "assets/images/portfolio/img-11.png",
-    "alt": "img 11"},
-    {"src" : "assets/images/portfolio/img-12.png",
-    "alt": "img 12"},
+    {
+        "src": "assets/images/portfolio/img-1.png",
+        "alt": "img 1"
+    },
+    {
+        "src": "assets/images/portfolio/img-2.png",
+        "alt": "img 2"
+    },
+    {
+        "src": "assets/images/portfolio/img-3.png",
+        "alt": "img 3"
+    },
+    {
+        "src": "assets/images/portfolio/img-4.png",
+        "alt": "img 4"
+    },
+    {
+        "src": "assets/images/portfolio/img-5.png",
+        "alt": "img 5"
+    },
+    {
+        "src": "assets/images/portfolio/img-6.png",
+        "alt": "img 6"
+    },
+    {
+        "src": "assets/images/portfolio/img-7.png",
+        "alt": "img 7"
+    },
+    {
+        "src": "assets/images/portfolio/img-8.png",
+        "alt": "img 8"
+    },
+    {
+        "src": "assets/images/portfolio/img-9.png",
+        "alt": "img 9"
+    },
+    {
+        "src": "assets/images/portfolio/img-10.png",
+        "alt": "img 10"
+    },
+    {
+        "src": "assets/images/portfolio/img-11.png",
+        "alt": "img 11"
+    },
+    {
+        "src": "assets/images/portfolio/img-12.png",
+        "alt": "img 12"
+    },
 ]
 
-const  makeRandomArr = (a, b) => Math.random() - 0.5;
-  
+const makeRandomArr = (a, b) => Math.random() - 0.5;
 
-const createElement = (tagName,className,src,alt) => {
-    let elem =  document.createElement(tagName)
-     if (className) {
-         elem.classList.add(className);
-     }
-     if (src) {
-         elem.src=src
-     }
-     if (alt) {
-         elem.alt=alt
-     }
-     return elem;
- }
- 
+
+const createElement = (tagName, className, src, alt) => {
+    let elem = document.createElement(tagName)
+    if (className) {
+        elem.classList.add(className);
+    }
+    if (src) {
+        elem.src = src
+    }
+    if (alt) {
+        elem.alt = alt
+    }
+    return elem;
+}
+
 
 let portfolioFilter = document.querySelector(".filter");
 let portfolioGallery = document.querySelector(".gallery")
 for (let img of portfolioImages) {
-    portfolioGallery.append(createElement("img","gallery__img",img.src,img.alt))
+    portfolioGallery.append(createElement("img", "gallery__img", img.src, img.alt))
 }
 
-portfolioFilter.addEventListener("click", function(event) {
+portfolioFilter.addEventListener("click", function (event) {
     let items = portfolioFilter.querySelectorAll(".filter__item")
-    if (event.target.tagName=="LI") {
+    if (event.target.tagName == "LI") {
         if (event.target.classList.contains("filter__item--active")) {
             return;
         }
-    for (let item of items) {
-        item.classList.remove("filter__item--active");
+        for (let item of items) {
+            item.classList.remove("filter__item--active");
 
-    }
+        }
 
-        if (event.target.textContent=="All") {
-            portfolioGallery.innerHTML="";
-            for (let img of portfolioImages) {            
-                portfolioGallery.append(createElement("img","gallery__img",img.src,img.alt))
+        if (event.target.textContent == "All") {
+            portfolioGallery.innerHTML = "";
+            for (let img of portfolioImages) {
+                portfolioGallery.append(createElement("img", "gallery__img", img.src, img.alt))
             }
         }
         else {
-            portfolioGallery.innerHTML="";
+            portfolioGallery.innerHTML = "";
             for (let img of portfolioImages.slice().sort(makeRandomArr)) {
-                
-                portfolioGallery.append(createElement("img","gallery__img",img.src,img.alt))
+
+                portfolioGallery.append(createElement("img", "gallery__img", img.src, img.alt))
             }
         }
         event.target.classList.add("filter__item--active")
@@ -94,9 +118,9 @@ portfolioFilter.addEventListener("click", function(event) {
 }
 )
 
-portfolioGallery.addEventListener("click", function(event) {
+portfolioGallery.addEventListener("click", function (event) {
     let images = portfolioGallery.querySelectorAll("img")
-    if (event.target.tagName=="IMG") {
+    if (event.target.tagName == "IMG") {
         for (let img of images) {
             if (img.classList.contains("gallery__img--active")) {
                 img.classList.remove("gallery__img--active");
@@ -105,6 +129,19 @@ portfolioGallery.addEventListener("click", function(event) {
         }
         event.target.classList.add("gallery__img--active");
 
+    }
+})
+
+let sliderContent = document.querySelector(".slider__content")
+sliderContent.addEventListener("click", evt => {
+
+    if (evt.target.closest(".phone-horizontal__base")) {
+        sliderContent.querySelector(".phone-horizontal__screen").classList.toggle("display-none")
+        return;
+    }
+    if (evt.target.closest(".phone-vertical__base")) {
+        sliderContent.querySelector(".phone-vertical__screen").classList.toggle("display-none")
+        return;
     }
 })
 
